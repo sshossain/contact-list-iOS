637eae88a67e2d08b270386c1e4e90eba9384d1a
sshossain <sshossain@gmail.com.br>
2014-01-15 16:43:08 -0200

    Adding support for contacts pictures Signed-off-by: sshossain <sshossain@gmail.com.br>

---
+++ ContatosNoMapaViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContatosNoMapaViewController.h
+//  ContatosIP67
+//
+//  Created by Soraya Hossain on 1/15/14.
+//  Copyright (c) 2014 ios4212. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContatosNoMapaViewController : UIViewController
+
+@end
---
+++ ContatosNoMapaViewController.m
@@ -0,0 +1,38 @@
+//
+//  ContatosNoMapaViewController.m
+//  ContatosIP67
+//
+//  Created by Soraya Hossain on 1/15/14.
+//  Copyright (c) 2014 ios4212. All rights reserved.
+//
+
+#import "ContatosNoMapaViewController.h"
+
+@interface ContatosNoMapaViewController ()
+
+@end
+
+@implementation ContatosNoMapaViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
